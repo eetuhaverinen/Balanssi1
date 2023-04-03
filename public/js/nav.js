7dc7086b9441ba91011629c13d0e0798fa88b215
ottokah <95294556+ottokah@users.noreply.github.com>
2023-04-03 23:31:43 +0300

    nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,28 +1,63 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 50,
-            sectionId = current.getAttribute('id')
+  // Loop through all the sections
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    const sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*="' + sectionId + '"]').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*="' + sectionId + '"]').classList.remove('active-link')
-        }
-    })
+    // Check if the section is currently visible
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      // Find the corresponding navigation item using the section ID
+      const navItem = document.querySelector('.nav__item.' + sectionId)
+
+      // Remove the active-link class from all the navigation items
+      document.querySelectorAll('.nav__item').forEach(item => {
+        item.classList.remove('active-link')
+      })
+
+      // Add the active-link class to the current navigation item
+      navItem.classList.add('active-link')
+    }
+  })
 }
+
+// Add event listener for click event on navigation links
+document.querySelectorAll('.nav__menu a').forEach(link => {
+  link.addEventListener('click', function() {
+    // Remove active-link class from all navigation links
+    document.querySelectorAll('.nav__menu a').forEach(link => {
+      link.classList.remove('active-link')
+    })
+
+    // Add active-link class to the clicked navigation link
+    this.parentElement.classList.add('active-link')
+  })
+})
+
 window.addEventListener('scroll', scrollActive)
 
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
-    const header = document.getElementById('header')
-    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+function scrollHeader() {
+  const header = document.getElementById('header')
+  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) {
+    header.classList.add('scroll-header')
+  } else {
+    header.classList.remove('scroll-header')
+  }
 }
 window.addEventListener('scroll', scrollHeader)
+
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('.nav__link');
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].classList.add("active-link");
+  }
+}
