86d3a050cfc17fe9a5bc48815aad8d362ff42479
Eetu Haverinen <eetu.haverinen@metropolia.fi>
2023-04-13 21:38:28 +0300

    tietojen päivitys profiili-sivulla

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,32 +1,70 @@
 'use strict';
-const API_URL = 'http://localhost:4000/api'; // change url when uploading to server
 
-const signupForm = document.querySelector('#signup-form');
+// const profileForm = document.getElementById("profileForm");
+// const submitButton = document.getElementById("submitButton");
 
-// login
-signupForm.addEventListener('submit', async (evt) => {
-  evt.preventDefault();
-  const data = serializeJson(signupForm);
-  console.log(JSON.stringify(data));
+// submitButton.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   const formData = new FormData(profileForm);
+//   const loader = document.getElementById("loader");
 
-  const fetchOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  };
+//   // Näytä loader-animaatio
+//   loader.style.display = "block";
 
-  const response = await fetch(API_URL + '/user/signup', fetchOptions);
+//   fetch("/profiili", {
+//     method: "POST",
+//     body: formData,
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.success) {
+//         // Piilota loader-animaatio
+//         loader.style.display = "none";
+//         // Näytä "Tiedot päivitetty" -viesti
+//         const message = document.getElementById("message");
+//         message.style.display = "block";
+//         setTimeout(() => {
+//           message.style.display = "none";
+//         }, 3000); // Piilota viesti 3 sekunnin kuluttua
+//       } else {
+//         // Piilota loader-animaatio
+//         loader.style.display = "none";
+//         alert("Päivitys epäonnistui!");
+//       }
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//       alert("Päivitys epäonnistui!");
+//     });
+// });
+// const API_URL = 'http://localhost:4000/api'; // change url when uploading to server
 
-  const json = await response.json();
-  console.log('login response', json);
-  if (!json.email) {
-    alert(json.message);
-  } else {
-    // save token
-    sessionStorage.setItem('token', json.token);
-    sessionStorage.setItem('email', json.email);
-    location.href = 'front.html';
-  }
-});
+// const signupForm = document.querySelector('#signup-form');
+
+// // login
+// signupForm.addEventListener('submit', async (evt) => {
+//   evt.preventDefault();
+//   const data = serializeJson(signupForm);
+//   console.log(JSON.stringify(data));
+
+//   const fetchOptions = {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   };
+
+//   const response = await fetch(API_URL + '/user/signup', fetchOptions);
+
+//   const json = await response.json();
+//   console.log('login response', json);
+//   if (!json.email) {
+//     alert(json.message);
+//   } else {
+//     // save token
+//     sessionStorage.setItem('token', json.token);
+//     sessionStorage.setItem('email', json.email);
+//     location.href = 'front.html';
+//   }
+// });
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 const Story = require('../models/StoryModel');
+const User = require('../models/userModel');
 
 // @desc    Landing page
 // @route   GET /
@@ -64,18 +65,47 @@ router.get('/viestit', ensureAuth, async (req, res) => {
   }
 });
 
-router.get('/profiili', ensureAuth, async (req, res) => {
+router.get('/profiili', ensureAuth, async (req, res, next) => {
   try {
     const decoded = jwt.verify(req.cookies.cookieToken, process.env.SECRET);
-    const stories = await Story.find({ user: decoded._id }).lean();
+    const user = await User.findById(decoded._id).lean();
     // const stories = await Story.find({}).lean();
     res.render('profile', {
       // username: decoded.username,
-      stories,
+      user,
     });
   } catch (err) {
     console.error(err);
     res.render('error/500');
+  }
+});
+
+router.post('/profiili', ensureAuth, async (req, res) => {
+  const {nimi, syntymaAika, pituus, paino, sukupuoli, leposyke, maksimisyke, BHbA1c} = req.body;
+
+  try {
+    // Validointi?
+    if ( !nimi || !pituus || !paino || !leposyke || !maksimisyke ) {
+      throw Error('Täytä pakolliset tiedot');
+    }
+    const decoded = jwt.verify(req.cookies.cookieToken, process.env.SECRET);
+    const user = await User.findByIdAndUpdate(decoded._id, {
+      nimi,
+      syntymaAika,
+      pituus,
+      paino,
+      sukupuoli,
+      leposyke,
+      maksimisyke,
+      BHbA1c
+    }, {new: true});
+    await user.save();
+
+    res.redirect('/profiili');
+
+  } catch (err) {
+    console.error(err);
+    res.json({success: false, errorMessage: err.message});
   }
 });
 
