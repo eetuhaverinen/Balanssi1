421e25406db98426dd18bca7022645c333b97c0f
Eetu Haverinen <eetu.haverinen@metropolia.fi>
2023-04-14 23:46:10 +0300

    sw aloitus

--- app.js
+++ app.js
@@ -82,3 +82,17 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+
+
+  if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('Service worker registered successfully:', registration);
+        })
+        .catch(error => {
+          console.log('Service worker registration failed:', error);
+        });
+    });
+  };
---
+++ public/js/sw.js
@@ -0,0 +1,94 @@
+
+// Install event listener
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache')
+        .then((cache) => {
+          return cache.addAll([
+            '/',
+            '/css/index.css',
+            '/css/style.css',
+            '/css/Login.css',
+            '/css/diary.css',
+            '/css/nav.css',
+            '/css/loader.css',
+            '/js/chart.js',
+            '/js/diary.js',
+            '/js/loader.js',
+            '/js/login.js',
+            '/js/nav.js',
+            '/js/serialize.js',
+            '/js/signup.js',
+            '/login.hbs',
+            '/home.hbs',
+            '/main.hbs',
+            '/_header_login.hbs',
+            '/_header_stories.hbs',
+            '/_header.hbs',
+            '/chart.hbs',
+            '/loading.hbs',
+            '/profiili.hbs',
+            '/verensokeriAdd.hbs',
+            '/verensokeriarvot.hbs',
+          ]);
+        })
+    );
+  });
+  
+  self.addEventListener('install', async e => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+    return self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', e => {
+    self.clients.claim();
+  });
+  
+  self.addEventListener('fetch', async e => {
+    const req = e.request;
+    const url = new URL(req.url);
+  
+    if (url.origin === location.origin) {
+      e.respondWith(cacheFirst(req));
+    } else {
+      e.respondWith(networkAndCache(req));
+    }
+  });
+  
+  async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cached = await cache.match(req);
+    return cached || fetch(req);
+  }
+  
+  async function networkAndCache(req) {
+    const cache = await caches.open(cacheName);
+    try {
+      const fresh = await fetch(req);
+      await cache.put(req, fresh.clone());
+      return fresh;
+    } catch (e) {
+      const cached = await cache.match(req);
+      return cached;
+    }
+  }
+
+//   // Fetch event listener
+//   self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//       caches.match(event.request)
+//         .then((response) => {
+//           return response || fetch(event.request);
+//         })
+//     );
+//   });
+  
+//   // Push event listener
+//   self.addEventListener('push', (event) => {
+//     const data = event.data.json();
+//     self.registration.showNotification(data.title, {
+//       body: data.message,
+//       icon: '/images/notification-icon.png'
+//     });
+//   });
