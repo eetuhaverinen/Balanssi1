1c644f6ff4e605ed35ad658edb42cd7ea09d909a
ottokah <95294556+ottokah@users.noreply.github.com>
2023-04-19 01:52:13 +0300

    j

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -1,22 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-  (function () {
-    const toggleSwitch = document.querySelector("#toggleDarkMode");
+const darkButton = document.getElementById('dark')
+const lightButton = document.getElementById('light')
+const solarButton = document.getElementById('solar')
+const body = document.body
+const styleSheet = document.getElementById('main-stylesheet')
+const solarStyleSheet = document.getElementById('solarmode-stylesheet')
 
-    // Automatically handle dark mode
-    if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
-      toggleSwitch.checked = true;
-      document.documentElement.setAttribute("data-mode", "dark");
-    }
+const theme = localStorage.getItem('theme');
 
-    // Manually handle dark mode
-    toggleSwitch.addEventListener("change", (event) => {
-      if (event.target.checked) {
-        document.documentElement.setAttribute("data-mode", "dark");
-        document.getElementById("darkmode-stylesheet").href = "/css/dark-mode.css";
-      } else {
-        document.documentElement.removeAttribute("data-mode");
-        document.getElementById("main-stylesheet").href = "/css/index.css";
-      }
-    });
-  })();
-});
+if (theme === 'dark') {
+  body.classList.add('dark-mode');
+  styleSheet.setAttribute('href', '/css/dark-mode.css');
+} else if (theme === 'solar') {
+  body.classList.add('solar');
+  solarButton.innerText = 'Normalize';
+  styleSheet.setAttribute('href', '/css/solar-mode.css');
+} else {
+  body.classList.add('light-mode');
+}
+
+darkButton.addEventListener('click', () => {
+  body.classList.replace('light-mode', 'dark-mode');
+  styleSheet.setAttribute('href', '/css/dark-mode.css');
+  localStorage.setItem('theme', 'dark');
+})
+
+lightButton.addEventListener('click', () => {
+  body.classList.replace('dark-mode', 'light-mode');
+  styleSheet.setAttribute('href', '/css/index.css');
+  localStorage.setItem('theme', 'light');
+})
+
+solarButton.addEventListener('click', () => {
+  if (body.classList.contains('solar')) {
+    body.classList.remove('solar');
+    solarButton.innerText = 'Solarize';
+    styleSheet.setAttribute('href', '/css/index.css');
+    localStorage.setItem('theme', 'light');
+  } else {
+    body.classList.add('solar');
+    solarButton.innerText = 'Normalize';
+    styleSheet.setAttribute('href', '/css/solar-mode.css');
+    localStorage.setItem('theme', 'solar');
+  }
+})
