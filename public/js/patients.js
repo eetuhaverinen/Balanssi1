61b32af1242d1579d239b1cb3f06e0a2e825643a
ottokah <95294556+ottokah@users.noreply.github.com>
2023-04-23 12:54:50 +0300

    potilaslista

--- public/js/patients.js
+++ public/js/patients.js
@@ -1,55 +1,42 @@
 $(document).ready(function () {
-  const searchInput = $("#searchBar");
-  const patientRows = $(".patient-name").parent().parent();
+  const patients = $('.patient-name');
+  const measurementTable = $('#measurementTable');
+  const searchBar = $('#searchBar');
 
-  // Search functionality
-  searchInput.on("keyup", function () {
-      const searchTerm = searchInput.val().toLowerCase();
+  const storiesData = JSON.parse($('#stories-data').text());
 
-      patientRows.each(function () {
-          const name = $(this).find(".patient-name").text().toLowerCase();
-          if (name.includes(searchTerm)) {
-              $(this).show();
-          } else {
-              $(this).hide();
-          }
-      });
+  patients.on('click', function (event) {
+    event.preventDefault();
+    console.log('Patient clicked:', $(this).text());
+    const patientId = $(this).data('patient-id');
+    const patientStories = storiesData.filter((story) => story.user._id === patientId);
+
+    let tableContent = '';
+    patientStories.forEach((story) => {
+      tableContent += `
+        <tr>
+          <td>${new Date(story.createdAt).toLocaleString()}</td>
+          <td>${story.mmolPerL}</td>
+          <td>${story.feeling}</td>
+          <td>${story.GHH}</td>
+          <td>${story.sport} ${story.sportDuration} min</td>
+          <td>${story.body}</td>
+        </tr>
+      `;
+    });
+
+    measurementTable.html(tableContent);
   });
 
-  // Handle click event on patient name
-  $('.patient-name').on('click', function (e) {
-      e.preventDefault();
-
-      // Get patient ID and name from the clicked link
-      const patientId = $(this).data('patient-id');
-      const patientName = $(this).text();
-
-      // Update the patient name above the measurement table
-      $('#patientName').text(patientName);
-
-      // Fetch patient measurements data
-      fetchPatientStories(patientId).then((stories) => {
-          // Populate the measurement table with the fetched data
-          const table = $('.measurementTable');
-          table.empty(); // Clear the table
-
-          // Add table headers and populate the table with the stories data
-          // Adapt the header names and data properties based on your specific data structure
-          table.append('<thead><tr><th>Date</th><th>mmolPerL</th><th>GHH</th><th>Sport Duration</th></tr></thead>');
-          const tbody = $('<tbody></tbody>');
-          stories.forEach((story) => {
-              const createdAt = moment(story.createdAt).format('MMM D, YYYY, h:mm:ss a');
-              tbody.append(`<tr><td>${createdAt}</td><td>${story.mmolPerL}</td><td>${story.GHH}</td><td>${story.sportDuration}</td></tr>`);
-          });
-          table.append(tbody);
-      });
+  searchBar.on('input', function (event) {
+    const searchText = $(this).val().toLowerCase();
+    patients.each(function () {
+      const patientName = $(this).text().toLowerCase();
+      if (patientName.includes(searchText)) {
+        $(this).closest('tr').show();
+      } else {
+        $(this).closest('tr').hide();
+      }
+    });
   });
-
-  // Function to fetch patient stories data (replace with your API endpoint)
-  async function fetchPatientStories(patientId) {
-      // Replace with the actual API URL and parameters as needed
-      const response = await fetch(`/api/stories?patientId=${patientId}`);
-      const data = await response.json();
-      return data;
-  }
 });
