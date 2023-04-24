c72c21651b1cca06f26e3fad52f2f984c625d64c
Eetu Haverinen <eetu.haverinen@metropolia.fi>
2023-04-24 17:18:59 +0300

    css

--- public/js/nav.js
+++ public/js/nav.js
@@ -27,3 +27,12 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('.nav__link');
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].classList.add("active-link");
+  }
+}
+
